3ee0aa3da4258b9d592d13744378d5ea7d56c420
sahinme <iamfreeadmiral@gmail.com>
2020-07-11 16:11:59 +0300

    /

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 const withImages = require('next-images');
 
-module.exports = withImages({
-    distDir: "_next",
-    generateBuildId: async () => {
-        if (process.env.BUILD_ID) {
-            return process.env.BUILD_ID;
-        } else {
-            return `${new Date().getTime()}`;
-        }
-    },
-}) 
+module.exports = withImages() 
