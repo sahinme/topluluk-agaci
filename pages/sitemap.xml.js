6e022c46486d93c7c0ea7ed29e2fe8f8cbab5465
alisahindev <alisahindev@gmail.com>
2020-07-17 21:09:34 +0300

    .

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -4,28 +4,28 @@ import { API_URL } from "../lib/services"
 
 export const sitemapXML = data => {
 
-    let projectsXML = "";
-    let comUrlsXml = "";
-    data.map(post => {
-        const projectURL = `https://saalla.com/${post.cSlug}/${post.pSlug}`;
-        projectsXML += `
+  let projectsXML = "";
+  let comUrlsXml = "";
+  data.map(post => {
+    const projectURL = `https://saalla.com/${post.cSlug}/${post.pSlug}`;
+    projectsXML += `
       <url>
         <loc>${projectURL}</loc>
         <priority>0.50</priority>
       </url>`;
-    });
+  });
 
-    data.map(post => {
-        const comUrls = `https://saalla.com/t/${post.cSlug}`;
-        comUrlsXml += `
+  data.map(post => {
+    const comUrls = `https://saalla.com/t/${post.cSlug}`;
+    comUrlsXml += `
       <url>
         <loc>${comUrls}</loc>
         <priority>0.50</priority>
       </url>`;
-    });
+  });
 
 
-    return `<?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${projectsXML}
       ${comUrlsXml}
@@ -34,16 +34,28 @@ export const sitemapXML = data => {
 
 class Sitemap extends React.Component {
 
-    static async getInitialProps({ res }) {
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-        const data = await axios
-            .get(`${API_URL}/post/get-slugs`)
-            .then(response => response.data);
+  /*  static async getInitialProps({ res }) {
+     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+     const data = await axios
+       .get(`${API_URL}/post/get-slugs`)
+       .then(response => response.data);
+ 
+     res.setHeader("Content-Type", "text/xml");
+     res.write(sitemapXML(data));
+     res.end();
+   } */
+}
 
-        res.setHeader("Content-Type", "text/xml");
-        res.write(sitemapXML(data));
-        res.end();
-    }
+export async function getServerSideProps({ res }) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  const data = await axios
+    .get(`${API_URL}/post/get-slugs`)
+    .then(response => response.data);
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemapXML(data));
+  res.end();
+  return { props: { data } }
 }
 
 export default Sitemap;
