05ce3861e853dd0abacba480360479d9027ad707
alisahindev <alisahindev@gmail.com>
2020-07-27 22:40:46 +0300

    .

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
-const withImages = require('next-images');
+const withCSS = require("@zeit/next-css");
+require('dotenv').config()
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
-module.exports = withImages() 
+const withImages = require('next-images')
+
+module.exports = withCSS(withImages({
+    inlineImageLimit: 16384,
+    webpack(config, options) {
+        config.plugins = config.plugins || [];
+        config.plugins = [
+            ...config.plugins,
+
+            // Read the .env file
+            new Dotenv({
+                path: path.join(__dirname, '.env'),
+                systemvars: true
+            })
+        ];
+        return config
+    }
+}));
