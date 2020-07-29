7af4083c9fa6c41cd11f8dbf3ec44928486867e5
alisahindev <alisahindev@gmail.com>
2020-07-29 18:09:26 +0300

    .

--- pages/nedir.jsx
+++ pages/nedir.jsx
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import MainLayout from '../components/mainLayout';
+import { Button } from '@material-ui/core';
+import Link from 'next/link';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,7 +15,9 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary
   },
   image: {
-    width: '50%'
+    width: '100%',
+    maxWidth: '100%',
+    height: 'auto'
   },
   item_grid: {
     padding: '0px !important'
@@ -49,6 +53,15 @@ export default function AboutUs() {
           <Grid style={{ padding: 0 }} item sm={12} xs={12}>
             <Paper className={classes.paper}>
               <img src="/puan.jpg" className={classes.image} />
+            </Paper>
+          </Grid>
+          <Grid style={{ padding: 0 }} item sm={12} xs={12}>
+            <Paper className={classes.paper}>
+              <Link href="/kaydol">
+                <Button variant="contained" color="primary">
+                  Keşfetmeye Başla
+                </Button>
+              </Link>
             </Paper>
           </Grid>
         </Grid>
