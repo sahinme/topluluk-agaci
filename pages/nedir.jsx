b2ed947d7203ced300fa3857a48824ca90422670
alisahindev <alisahindev@gmail.com>
2020-07-29 17:28:00 +0300

    .

--- pages/nedir.jsx
+++ pages/nedir.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import MainLayout from '../components/mainLayout';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,29 +24,35 @@ export default function AboutUs() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid style={{ padding: 0 }} item sm={12} xs={12}>
-          <Paper className={classes.paper}>
-            <img src="/topluluk.jpg" className={classes.image} />
-          </Paper>
+    <MainLayout>
+      <div className={classes.root}>
+        <Grid
+          style={{ height: '100vh', backgroundColor: 'white' }}
+          container
+          spacing={3}
+        >
+          <Grid style={{ padding: 0 }} item sm={12} xs={12}>
+            <Paper className={classes.paper}>
+              <img src="/topluluk.jpg" className={classes.image} />
+            </Paper>
+          </Grid>
+          <Grid style={{ padding: 0 }} item sm={12} xs={12}>
+            <Paper className={classes.paper}>
+              <img src="/topluluk-2.jpg" className={classes.image} />
+            </Paper>
+          </Grid>
+          <Grid style={{ padding: 0 }} item sm={12} xs={12}>
+            <Paper className={classes.paper}>
+              <img src="/moderator.jpg" className={classes.image} />
+            </Paper>
+          </Grid>
+          <Grid style={{ padding: 0 }} item sm={12} xs={12}>
+            <Paper className={classes.paper}>
+              <img src="/puan.jpg" className={classes.image} />
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid style={{ padding: 0 }} item sm={12} xs={12}>
-          <Paper className={classes.paper}>
-            <img src="/topluluk-2.jpg" className={classes.image} />
-          </Paper>
-        </Grid>
-        <Grid style={{ padding: 0 }} item sm={12} xs={12}>
-          <Paper className={classes.paper}>
-            <img src="/moderator.jpg" className={classes.image} />
-          </Paper>
-        </Grid>
-        <Grid style={{ padding: 0 }} item sm={12} xs={12}>
-          <Paper className={classes.paper}>
-            <img src="/puan.jpg" className={classes.image} />
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </MainLayout>
   );
 }
